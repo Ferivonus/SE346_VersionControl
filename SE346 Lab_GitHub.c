e7c96c12efad93888f03747457e7c07cd1e8afee
Ferivonus <ferivonus@hotmail.com>
2022-05-13 01:47:48 +0300

    v 1.2

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -8,8 +8,7 @@ int main()
 
 	printf("Z.Yaren Oguz,123456,SE\n");
 
-	printf("Ozge Tekin,987654,SE\n");
-
 	printf("Fahrettin Basturk, 18243610023, ISE\n"); // have a nice days ✧･ﾟ: *✧･ﾟ:* 　 *:･ﾟ✧*:･ﾟ✧ :3
+
 
 }
